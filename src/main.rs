e95e4e6981cc717db91eafa215a8ff41bae9a41f
Li Xin Rui <li.xinrui3@gmail.com>
2023-02-04 17:06:10 -0500

    added notion search on all objects

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ async fn main() {
     dotenv().ok();
     let notion_secret = std::env::var("NOTION_SECRET").expect("Notion Secret not set.");
     let notion_db = std::env::var("NOTION_DATABASE_ID").expect("Notion DB Id not set.");
-    
     let app_state = AppState {
         notion_secret: TokenWrapper(notion_secret),
         notion_db,
