ddedd67aa3316e331e3d779ebf180ae3585ca109
Li Xin Rui <li.xinrui3@gmail.com>
2023-02-16 18:34:43 -0500

    made oauth redirect work

--- src/utilities/token_wrapper.rs
+++ src/utilities/token_wrapper.rs
@@ -1,5 +1,23 @@
+use oauth2::{
+    Client, 
+    StandardErrorResponse, 
+    basic::{BasicClient, BasicErrorResponseType, BasicTokenType}, 
+    StandardTokenResponse, EmptyExtraTokenFields, StandardRevocableToken, RevocationErrorResponseType, StandardTokenIntrospectionResponse,
+    CsrfToken, PkceCodeChallenge, RedirectUrl,
+    RevocationUrl, Scope, TokenUrl, PkceCodeVerifier,
+};
+
 #[derive(Clone)]
 pub struct NotionSecret(pub String);
 
 #[derive(Clone)]
 pub struct TypesenseSecret(pub String);
+
+#[derive(Clone)]
+pub struct PKCECodeVerifier(pub String);
+
+#[derive(Clone)]
+pub struct CSRFToken(pub String);
+
+#[derive(Clone)]
+pub struct GoogleAuthClient(pub Client<StandardErrorResponse<BasicErrorResponseType>, StandardTokenResponse<EmptyExtraTokenFields, BasicTokenType>, BasicTokenType, StandardTokenIntrospectionResponse<EmptyExtraTokenFields, BasicTokenType>, StandardRevocableToken, StandardErrorResponse<RevocationErrorResponseType>>);
