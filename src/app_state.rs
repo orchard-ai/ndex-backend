fb143b98a2324409c9db57a3fe5b21102c5f2cc1
Li Xin Rui <li.xinrui3@gmail.com>
2023-02-19 17:50:28 -0500

    added app state for oauth client, next step is to write the middleware for it

--- src/app_state.rs
+++ src/app_state.rs
@@ -1,30 +1,29 @@
 use crate::utilities::token_wrapper::{
-    NotionSecret, TypesenseSecret, PKCECodeVerifier, CSRFToken, GoogleAuthClient,
+    NotionSecret, TypesenseSecret, PkceCodeVerifierWrapper, CsrfTokenWrapper,
 };
-use oauth2::{Client, StandardErrorResponse, basic::{BasicClient, BasicErrorResponseType, BasicTokenType}, StandardTokenResponse, EmptyExtraTokenFields, StandardRevocableToken, RevocationErrorResponseType, StandardTokenIntrospectionResponse};
-
-
+use oauth2::basic::BasicClient;
+use oauth2::{PkceCodeVerifier, CsrfToken};
 use axum::extract::FromRef;
 
 #[derive(Clone, FromRef)]
 pub struct AppState {
     pub typesense_secret: TypesenseSecret,
     pub notion_secret: NotionSecret,
-    pub pkce_code_verifier: Option<PKCECodeVerifier>,
-    pub csrf_state: Option<CSRFToken>,
-    pub google_auth_client: Option<Client<StandardErrorResponse<BasicErrorResponseType>, StandardTokenResponse<EmptyExtraTokenFields, BasicTokenType>, BasicTokenType, StandardTokenIntrospectionResponse<EmptyExtraTokenFields, BasicTokenType>, StandardRevocableToken, StandardErrorResponse<RevocationErrorResponseType>>>,
+    pub pkce_code_verifier: Option<PkceCodeVerifierWrapper>,
+    pub csrf_state: Option<CsrfTokenWrapper>,
+    pub google_auth_client: Option<BasicClient>,
 }
 
 impl AppState {
-    pub fn set_pkce_verifier(&mut self, pkce_code_verifier: PKCECodeVerifier) {
-        self.pkce_code_verifier = Some(pkce_code_verifier);
+    pub fn set_pkce_verifier(&mut self, pkce_code_verifier: PkceCodeVerifier) {
+        self.pkce_code_verifier = Some(PkceCodeVerifierWrapper(pkce_code_verifier.secret().to_string()));
     }
 
-    pub fn set_csrf_state(&mut self, csrf_state: CSRFToken) {
-        self.csrf_state = Some(csrf_state);
+    pub fn set_csrf_state(&mut self, csrf_state: CsrfToken) {
+        self.csrf_state = Some(CsrfTokenWrapper(csrf_state.secret().to_string()));
     }
 
-    pub fn set_google_auth_client(&mut self, google_auth_client: Client<StandardErrorResponse<BasicErrorResponseType>, StandardTokenResponse<EmptyExtraTokenFields, BasicTokenType>, BasicTokenType, StandardTokenIntrospectionResponse<EmptyExtraTokenFields, BasicTokenType>, StandardRevocableToken, StandardErrorResponse<RevocationErrorResponseType>>) {
+    pub fn set_google_auth_client(&mut self, google_auth_client: BasicClient) {
         self.google_auth_client = Some(google_auth_client);
     }
 }
--- src/router.rs
+++ src/router.rs
@@ -15,8 +15,9 @@ use crate::{
 use axum::{
     routing::{get, post},
     Router,
+    middleware::AddExtension
 };
-
+use tower::builder::ServiceBuilder;
 pub fn create_router(app_state: AppState) -> Router {
     Router::new()
         .route("/", get(root))
