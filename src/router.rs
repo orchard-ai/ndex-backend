7658df67e144208bfa9cf9c6e1c6e307b27e2b63
Li Xin Rui <li.xinrui3@gmail.com>
2023-03-19 17:22:01 -0400

    added cors

--- src/router.rs
+++ src/router.rs
@@ -1,23 +1,24 @@
 use crate::{
     app_state::AppState,
     routes::{
-        notion::{
-            search::search_all, 
-            retrieve_blocks::block_query,
-        },
-        typesense::{
-            schema_control::{create_document_schema, delete_schema, retrieve_all_schema},
-            index::{batch_index, single_index},
-        },
-        login::{google_auth::{google_auth, google_auth_sucess}},
         google::retrieve_calendar::retrieve_calendar_list,
+        login::google_auth::{google_auth, google_auth_sucess},
+        notion::{retrieve_blocks::block_query, search::search_all},
+        typesense::{
+            index::{batch_index, single_index},
+            schema_control::{create_document_schema, delete_schema, retrieve_all_schema},
+        },
     },
 };
 use axum::{
     routing::{get, post},
     Router,
 };
+use http::HeaderValue;
+use tower_http::cors::CorsLayer;
 pub fn create_router(app_state: AppState) -> Router {
+    let cors =
+        CorsLayer::new().allow_origin("https://localhost:5173".parse::<HeaderValue>().unwrap());
     Router::new()
         .route("/", get(root))
         .route("/google/auth", get(google_auth))
@@ -25,12 +26,19 @@ pub fn create_router(app_state: AppState) -> Router {
         .route("/google/calendar", get(retrieve_calendar_list))
         .route("/notion/search_notion", get(search_all))
         .route("/notion/retrieve_notion_blocks", post(block_query))
-        .route("/typesense/create_typesense_schema", get(create_document_schema))
+        .route(
+            "/typesense/create_typesense_schema",
+            get(create_document_schema),
+        )
         .route("/typesense/delete_typesense_schema", get(delete_schema))
-        .route("/typesense/retrieve_typesense_schema", get(retrieve_all_schema))
+        .route(
+            "/typesense/retrieve_typesense_schema",
+            get(retrieve_all_schema),
+        )
         .route("/typesense/batch_index", get(batch_index))
         .route("/typesense/single_index", post(single_index))
         .with_state(app_state)
+        .layer(cors)
 }
 
 async fn root() -> &'static str {
