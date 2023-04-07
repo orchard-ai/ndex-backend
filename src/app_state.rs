76df95c77f9d763f7386fc2f0e0255aebd7d469c
Li Xin Rui <li.xinrui3@gmail.com>
2023-04-07 15:00:09 -0700

    reformatted appstate for less bugs

--- src/app_state.rs
+++ src/app_state.rs
@@ -12,13 +12,31 @@ use oauth2::{AccessToken, CsrfToken, PkceCodeVerifier};
 pub struct AppState {
     pub typesense_secret: TypesenseSecret,
     pub notion_secret: NotionSecret,
-    pub google_auth_client_wrapper: Arc<Mutex<Option<BasicClient>>>,
-    pub pkce_code_verifier_wrapper: Arc<Mutex<Option<PkceCodeVerifierWrapper>>>,
-    pub csrf_state_wrapper: Arc<Mutex<Option<CsrfTokenWrapper>>>,
-    pub google_access_code_wrapper: Arc<Mutex<Option<GoogleAccessCodeWrapper>>>,
+    google_auth_client_wrapper: Arc<Mutex<Option<BasicClient>>>,
+    pkce_code_verifier_wrapper: Arc<Mutex<Option<PkceCodeVerifierWrapper>>>,
+    csrf_state_wrapper: Arc<Mutex<Option<CsrfTokenWrapper>>>,
+    google_access_code_wrapper: Arc<Mutex<Option<GoogleAccessCodeWrapper>>>,
 }
 
 impl AppState {
+    pub fn new(
+        typesense_secret: TypesenseSecret,
+        notion_secret: NotionSecret,
+        google_auth_client_wrapper: Arc<Mutex<Option<BasicClient>>>,
+        pkce_code_verifier_wrapper: Arc<Mutex<Option<PkceCodeVerifierWrapper>>>,
+        csrf_state_wrapper: Arc<Mutex<Option<CsrfTokenWrapper>>>,
+        google_access_code_wrapper: Arc<Mutex<Option<GoogleAccessCodeWrapper>>>,
+    ) -> Self {
+        Self {
+            typesense_secret,
+            notion_secret,
+            google_auth_client_wrapper,
+            pkce_code_verifier_wrapper,
+            csrf_state_wrapper,
+            google_access_code_wrapper,
+        }
+    }
+
     pub fn set_pkce_verifier(&mut self, pkce_code_verifier: PkceCodeVerifier) {
         let mut pkce_code_verifier_wrapper = self.pkce_code_verifier_wrapper.lock().unwrap();
         *pkce_code_verifier_wrapper = Some(PkceCodeVerifierWrapper(
@@ -39,5 +57,35 @@ impl AppState {
     pub fn set_google_access_code(&mut self, google_access_token: AccessToken) {
         let mut google_access_token_wrapper = self.google_access_code_wrapper.lock().unwrap();
         *google_access_token_wrapper = Some(GoogleAccessCodeWrapper(google_access_token));
+    }
+
+    pub fn get_google_client(&self) -> BasicClient {
+        self.google_auth_client_wrapper
+            .lock()
+            .unwrap()
+            .clone()
+            .unwrap()
+    }
+
+    pub fn get_google_access_code(&self) -> String {
+        self.google_access_code_wrapper
+            .lock()
+            .unwrap() // or use expect() to provide a custom error message
+            .as_ref()
+            .map(|wrapper| wrapper.clone())
+            .unwrap()
+            .0
+            .secret()
+            .to_owned()
+    }
+
+    pub fn get_pkce_verifier(&self) -> String {
+        self.pkce_code_verifier_wrapper
+            .lock()
+            .unwrap()
+            .clone()
+            .unwrap()
+            .0
+            .to_owned()
     }
 }
