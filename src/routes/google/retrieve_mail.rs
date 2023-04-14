fce6dad9b92afd499c50988b27d0646229b689c4
Li Xin Rui <li.xinrui3@gmail.com>
2023-04-14 17:01:46 -0400

    recursive search of an user's entire gmail inbox

--- src/routes/google/retrieve_mail.rs
+++ src/routes/google/retrieve_mail.rs
@@ -1,3 +1,4 @@
+use crate::models::gmail::Message;
 use crate::{app_state::AppState, models::gmail::MessagesList};
 
 use axum::response::IntoResponse;
@@ -7,48 +8,51 @@ use http::StatusCode;
 pub async fn retrieve_messages_list(State(state): State<AppState>) -> impl IntoResponse {
     let access_code = state.get_google_access_code();
     let client = reqwest::Client::new();
-    let response = client
-        .get("https://gmail.googleapis.com/gmail/v1/users/me/messages?maxResults=500")
-        .bearer_auth(&access_code)
-        .send()
-        .await
-        .unwrap();
-    let messages: MessagesList = response.json().await.unwrap();
-    get_message("test".to_string(), access_code).await;
-    // dbg!(&messages);
-    // if let Some(next_page) = &messages.next_page_token {
-    //     let next_url = format!(
-    //         "https://gmail.googleapis.com/gmail/v1/users/me/messages?maxResults=500&pageToken={}",
-    //         next_page
-    //     );
-    //     dbg!(next_page);
-    //     dbg!(&next_url);
-    //     let response = client
-    //         .get(next_url)
-    //         .bearer_auth(&access_code)
-    //         .send()
-    //         .await
-    //         .unwrap();
-    //     let next_messages: MessagesList = response.json().await.unwrap();
-    //     dbg!(&next_messages);
-    //     return (StatusCode::OK, Json(vec![messages, next_messages]));
-    // }
-    (StatusCode::OK, Json(vec![messages]))
+    let mut cursor: Option<String> = None;
+    let mut message_list: Vec<Message> = vec![];
+    loop {
+        let next_url: String;
+        if let Some(page_cursor) = cursor {
+            next_url = format!(
+            "https://gmail.googleapis.com/gmail/v1/users/me/messages?maxResults=500&pageToken={}",
+            page_cursor
+            );
+        } else {
+            next_url = "https://gmail.googleapis.com/gmail/v1/users/me/messages?maxResults=500"
+                .to_string();
+        }
+        let response = client
+            .get(next_url)
+            .bearer_auth(&access_code)
+            .send()
+            .await
+            .unwrap();
+        let messages_list: MessagesList = response.json().await.unwrap();
+        let next_cursor = messages_list.next_page_token.clone();
+        message_list.extend(messages_list.messages);
+        if let Some(next_page_cursor) = &next_cursor {
+            cursor = Some(next_page_cursor.to_owned());
+        } else {
+            break;
+        }
+    }
+    dbg!(&message_list.len());
+    (StatusCode::OK, Json(message_list))
 }
 
-pub async fn get_message(message_id: String, access_code: String) -> serde_json::Value {
-    let req_url = format!(
-        "https://gmail.googleapis.com/gmail/v1/users/me/messages/{}",
-        message_id
-    );
-    let client = reqwest::Client::new();
-    let response = client
-        .get(req_url)
-        .bearer_auth(&access_code)
-        .send()
-        .await
-        .unwrap();
-    let message = response.json().await.unwrap();
-    dbg!(&message);
-    return message;
-}
+// pub async fn get_message(message_id: String, access_code: String) -> serde_json::Value {
+//     let req_url = format!(
+//         "https://gmail.googleapis.com/gmail/v1/users/me/messages/{}",
+//         message_id
+//     );
+//     let client = reqwest::Client::new();
+//     let response = client
+//         .get(req_url)
+//         .bearer_auth(&access_code)
+//         .send()
+//         .await
+//         .unwrap();
+//     let message = response.json().await.unwrap();
+//     dbg!(&message);
+//     return message;
+// }
