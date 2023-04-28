78006679940a4f61203c0ffd0778876745889994
Li Xin Rui <li.xinrui3@gmail.com>
2023-04-28 18:22:35 -0400

    added parsed MIME format structs digest gmails

--- src/models/gmail.rs
+++ src/models/gmail.rs
@@ -15,3 +15,68 @@ pub struct Message {
     pub id: String,
     pub thread_id: String,
 }
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ParsedMail {
+    pub history_id: String,
+    pub id: String,
+    pub internal_date: String,
+    pub label_ids: Vec<String>,
+    pub payload: Payload,
+    pub size_estimate: i64,
+    pub snippet: String,
+    pub thread_id: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Payload {
+    pub body: Body,
+    pub filename: String,
+    pub headers: Vec<Header>,
+    pub mime_type: String,
+    pub part_id: String,
+    #[serde(default)]
+    pub parts: Vec<Part>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Body {
+    pub size: i64,
+    pub data: Option<String>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Header {
+    pub name: String,
+    pub value: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Part {
+    pub body: PartBody,
+    pub filename: String,
+    pub headers: Vec<PartHeader>,
+    pub mime_type: String,
+    pub part_id: String,
+    pub parts: Option<Vec<Part>>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct PartBody {
+    pub data: Option<String>,
+    pub size: i64,
+    pub attachment_id: Option<String>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct PartHeader {
+    pub name: String,
+    pub value: String,
+}
