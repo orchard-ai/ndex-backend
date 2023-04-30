d8553660df188d233cc97d8748b9d38722a9421e
Li Xin Rui <li.xinrui3@gmail.com>
2023-04-30 03:18:18 -0400

    finished parsing gmail mime format into typesense schema

--- src/routes/google/mod.rs
+++ src/routes/google/mod.rs
@@ -1,2 +1,14 @@
+use serde_derive::Deserialize;
+use serde_derive::Serialize;
 pub mod retrieve_calendar;
 pub mod retrieve_mail;
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct GMailUser {
+    id: String,
+    email_address: String,
+    messages_total: u64,
+    threads_total: u64,
+    history_id: String,
+}
