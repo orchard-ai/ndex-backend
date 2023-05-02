72025fb05ba45b728d34241cce58ae241a9cca1e
Li Xin Rui <li.xinrui3@gmail.com>
2023-05-02 16:20:22 -0400

    login and signup now follow api design and returns tokens correctly

--- src/utilities/errors.rs
+++ src/utilities/errors.rs
@@ -1,17 +1,16 @@
 use axum::{http::StatusCode, response::IntoResponse, Json};
 use serde_json::json;
+
 pub enum DbError {
     BadRequest(String),
     UserNotFound,
-    InternalServerError,
+    InternalServerError(String),
 }
 
 impl IntoResponse for DbError {
     fn into_response(self) -> axum::response::Response {
         let (status, error_message) = match self {
-            Self::InternalServerError => {
-                (StatusCode::INTERNAL_SERVER_ERROR, "Internal Server Error")
-            }
+            Self::InternalServerError(ref msg) => (StatusCode::INTERNAL_SERVER_ERROR, msg.as_str()),
             Self::BadRequest(ref msg) => (StatusCode::BAD_REQUEST, msg.as_str()),
             Self::UserNotFound => (StatusCode::NOT_FOUND, "User Not Found"),
         };
