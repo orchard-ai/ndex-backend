5ad989a3c7467194aa20494c0258838943637c9b
Philip Tam <philip.tam.code@gmail.com>
2023-05-22 18:28:45 -0400

    Send email working with signup

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use dotenv::dotenv;
 use ndex::{
     app_state::AppState,
     run,
-    utilities::token_wrapper::{NotionAccessSecret, NotionClientId, TypesenseSecret, GoogleClientId, GoogleClientSecret},
+    utilities::{token_wrapper::{NotionAccessSecret, NotionClientId, TypesenseSecret, GoogleClientId, GoogleClientSecret, NoReplyEmailId, NoReplySecret, NoReplyServer}},
 };
 use sqlx::postgres::PgPoolOptions;
 
@@ -24,6 +24,9 @@ async fn main() -> anyhow::Result<()> {
     let jwt_secret = std::env::var("JWT_SECRET").expect("JWT Secret not set.");
     let google_client_id = std::env::var("GOOGLE_CLIENT_ID").expect("Google client ID not set.");
     let google_client_secret = std::env::var("GOOGLE_CLIENT_SECRET").expect("Google client secret not set.");
+    let no_reply_email_id: String = std::env::var("NO_REPLY_EMAIL_ID").expect("No reply email not set.");
+    let no_reply_secret = std::env::var("NO_REPLY_SECRET").expect("No reply secret not set.");
+    let no_reply_server = std::env::var("NO_REPLY_SERVER").expect("No reply server not set.");
     let app_state = AppState::new(
         TypesenseSecret(typesense_secret),
         NotionClientId(notion_client_id),
@@ -36,8 +39,10 @@ async fn main() -> anyhow::Result<()> {
         Arc::new(Mutex::new(None)),
         GoogleClientId(google_client_id),
         GoogleClientSecret(google_client_secret),
+        NoReplyEmailId(no_reply_email_id),
+        NoReplySecret(no_reply_secret),
+        NoReplyServer(no_reply_server)
     );
-
     run(app_state).await?;
     Ok(())
 }
