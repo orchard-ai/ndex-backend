c2a8a7273fdd1528a7f4c2c0a84dbb2ef6ec67e9
jo6109072 <john.pan@bell.ca>
2023-05-23 23:12:42 -0400

    feat: Add confirmation link

---
+++ migrations/20230524025213_user-confirmation-fields.sql
@@ -0,0 +1,10 @@
+-- Add migration script here
+ALTER TABLE userdb.users ADD active BOOLEAN NOT NULL DEFAULT FALSE;
+
+CREATE TABLE userdb.confirmation_hash (
+    id SERIAL PRIMARY KEY,
+    FOREIGN KEY (user_id) REFERENCES userdb.users (id) ON DELETE CASCADE,
+    hash_key VARCHAR(255)
+    -- hash_key and user_id are not unique together, user might request another link if lost first one
+);
+
